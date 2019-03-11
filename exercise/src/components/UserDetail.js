0a446eb05c8347090e9af773ce977b13d0d9c57b
m-camacho <mario.camacho2@ymail.com>
2019-03-11 10:06:40 -0400

    User list page

--- exercise/src/components/UserDetail.js
+++ exercise/src/components/UserDetail.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
-export default class UserDetail extends Component {
-  render() {
-    console.log(this.props.match);
-    return (
-        <div>
-            User Detail
-        </div>
-    );
-  }
+class UserDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { selectedUser: null };
+    }
+
+    componentDidMount() {
+        const { userList, match, history } = this.props;
+        const found = userList.find(user => user.id == match.params.id);
+        if (found) {
+            this.setState({ selectedUser: found });
+        } else {
+            // logic for not found
+            history.push('/randomuser.me')
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>User Detail</h1> 
+                <Link to="/randomuser.me">Back</Link>
+            </div>
+        );
+    }
 }
 
+const mapStateToProps = (state) => ({ userList: state.userList });
+
+export default connect(mapStateToProps)(UserDetail);
--- exercise/src/components/UserList.js
+++ exercise/src/components/UserList.js
@@ -1,10 +1,45 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Table } from 'react-bootstrap';
 
-export default class UserList extends Component {
-  render() {
-    return (
-      <div>User List</div>
-    );
-  }
+class UserList extends Component {
+    selectUser(user) {
+        const { history } = this.props;
+        history.push(`/randomuser.me/${user.id}`);
+    }
+
+    render() {
+        const { userList } = this.props;
+        return (
+            <div>
+                <h1>User List</h1> 
+                <Table responsive>
+                    <thead>
+                        <tr>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>email</th>
+                        <th>Phone</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            userList.map(user => (
+                                <tr onClick={() => this.selectUser(user)} key={user.id}>
+                                    <td>{user.firstName}</td>
+                                    <td>{user.lastName}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.phone}</td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </Table>;
+            </div>
+        );
+    }
 }
 
+const mapStateToProps = (state) => ({ userList: state.userList });
+
+export default connect(mapStateToProps)(UserList);
