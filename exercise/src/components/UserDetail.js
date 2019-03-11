8273a7fefa95f8b6ad3f84de9cfa2989cf96c2e3
m-camacho <mario.camacho2@ymail.com>
2019-03-11 11:11:09 -0400

    User details page

--- exercise/src/components/UserDetail.js
+++ exercise/src/components/UserDetail.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
 
 class UserDetail extends Component {
     constructor(props) {
@@ -20,12 +21,23 @@ class UserDetail extends Component {
     }
 
     render() {
-        return (
-            <div>
-                <h1>User Detail</h1> 
-                <Link to="/randomuser.me">Back</Link>
+        const { selectedUser } = this.state;
+        return selectedUser ? (
+            <div style={{ padding: '20px' }}>
+                <h1>User Detail</h1>
+                <Card style={{ textAlign: 'center' }}>
+                    <Card.Img variant="top" src={selectedUser.picUrl} width="150"/>
+                    <Card.Body>
+                        <Card.Title><h3>{ `${selectedUser.firstName} ${selectedUser.lastName}` }</h3></Card.Title>
+                        <Card.Text>
+                            <div>{selectedUser.email}</div>
+                            <div>{selectedUser.phone}</div>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Link to="/randomuser.me">Back to Users list</Link>
             </div>
-        );
+        ) : null;
     }
 }
 
